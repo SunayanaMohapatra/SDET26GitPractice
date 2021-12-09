118b8719bac08623ef61bc95d612c3807396919c
sunay <sunay@NainasBook>
2021-12-09 16:27:04 +0530

    2nd one commit

--- src/test/java/com/crm/SDET26/Demo.java
+++ src/test/java/com/crm/SDET26/Demo.java
@@ -2,9 +2,17 @@ package com.crm.SDET26;
 
 public class Demo {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
-System.out.println("Hi all");
+         System.out.println("Hi all");
+          System.out.println("goood afternoon");
+          
+          System.out.println("Hi all");
+          System.out.println("goood afternoon");
+
+          System.out.println("Hi all");
+          System.out.println("goood afternoon");
 	}
 
 }
